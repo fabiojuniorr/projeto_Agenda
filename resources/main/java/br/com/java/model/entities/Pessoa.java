0e76b8ee90c344d46f1f6c2aea3582a4d8620668
Fabio <Fabio@Fabio-PC>
2014-04-26 00:08:08 -0300

    Implementação da Entidade Pessoa, com mapeamento JPA.

---
+++ resources/main/java/br/com/java/model/entities/Pessoa.java
@@ -0,0 +1,134 @@
+package br.com.java.model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+@Entity
+@Table(name = "Pessoa")
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	// autoIncremento
+	@Column(name = "IdPessoa", nullable = false)
+	private Integer idPessoa;
+
+	@Column(name = "Nome", nullable = false, length = 80)
+	private String nome;
+
+	@Column(name = "E-mail", nullable = false, length = 80)
+	private String email;
+
+	@Column(name = "Telefone", nullable = false, length = 15)
+	private String telefone;
+
+	@Column(name = "CPF", nullable = false, length = 14)
+	private String cpf;
+
+	@Column(name = "Data_De_Nasciemnto", nullable = false)
+	@Temporal(javax.persistence.TemporalType.DATE)
+	private Date dataDeNascimento;
+
+	@Column(name = "Data_De_Cadastro", nullable = false)
+	@Temporal(javax.persistence.TemporalType.DATE)
+	private Date dataDeCadastro;
+
+	public Pessoa() {
+		super();
+	}
+
+	public Integer getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Integer idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public Date getDataDeCadastro() {
+		return dataDeCadastro;
+	}
+
+	public void setDataDeCadastro(Date dataDeCadastro) {
+		this.dataDeCadastro = dataDeCadastro;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((idPessoa == null) ? 0 : idPessoa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (idPessoa == null) {
+			if (other.idPessoa != null)
+				return false;
+		} else if (!idPessoa.equals(other.idPessoa))
+			return false;
+		return true;
+	}
+
+}
--- resources/main/java/br/com/projeto/util/HibernateUtil.java
+++ resources/main/java/br/com/projeto/util/HibernateUtil.java
@@ -1,4 +1,4 @@
-package br.com.java.util;
+package br.com.projeto.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
